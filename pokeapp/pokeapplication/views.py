d910fe78a4190fef16bd960031e3f0d525c6c05a
David Viana <david.viana@germinare.org.br>
2022-10-20 06:59:38 -0300

    fixing urlconfg + change db.sqlite3

--- pokeapp/pokeapplication/views.py
+++ pokeapp/pokeapplication/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-x = 24
-y = 2
-
 def index(request):
-    return HttpResponse(f"Eita boy: {x + y}")
+    return HttpResponse("shhh calado")
