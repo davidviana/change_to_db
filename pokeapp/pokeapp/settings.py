d9119988d05687082267b2eb03a86995cb638306
David Viana <david.viana@germinare.org.br>
2022-10-21 16:08:13 -0300

    starting code style and structure of application

--- pokeapp/pokeapp/settings.py
+++ pokeapp/pokeapp/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "pokeapplication/static"),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- pokeapp/pokeapplication/urls.py
+++ pokeapp/pokeapplication/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
