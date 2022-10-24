ed4ea3b0747a9ece27c527b6434cb5163707f638
David Viana <david.viana@germinare.org.br>
2022-10-24 10:12:33 -0300

    finish commit

--- pokeapp/pokeapplication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', views.index, name='index'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
--- pokeapp/pokeapplication/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    a = 2 + 2
-    cars = ["Honda", "Toyota", "Ferrari"]
-    context = {"result": a, "cars": cars}
-
-    return render(request, "index.html", context)
